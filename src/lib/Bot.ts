e5abbf2fd2b8e214e512731d580a1df272282f90
binoy638 <binoy638@gmail.com>
2022-03-19 23:55:09 +0530

    bug fix

--- src/lib/Bot.ts
+++ src/lib/Bot.ts
@@ -23,7 +23,7 @@ export class Bot extends Client {
         Intents.FLAGS.GUILD_MESSAGES,
         Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
         Intents.FLAGS.GUILD_VOICE_STATES,
-        Intents.FLAGS.GUILD_MEMBERS,
+        // Intents.FLAGS.GUILD_MEMBERS,
       ],
     });
 
